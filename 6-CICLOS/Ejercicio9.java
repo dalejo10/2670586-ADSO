07b038b8a1629b7ca305ea3b845c7958c9e80325
Fast <ejemplo.example.com>
2023-06-15 20:38:33 -0500

    Ejercicios resueltos

--- 6-CICLOS/Ejercicio9.java
+++ 6-CICLOS/Ejercicio9.java
@@ -3,7 +3,7 @@ public class Ejercicio9{
     public static void main(String[] args){
         Scanner entrada = new Scanner(System.in);
 
-         System.out.print("Ingrese numero 1: ");
+        System.out.print("Ingrese un numero entero: ");
         int numero = entrada.nextInt();
 
         int contador = 0;
