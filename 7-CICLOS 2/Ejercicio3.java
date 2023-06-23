8671f9cbdfa394b94e293d49716307b40b6b8fb4
Fast <ejemplo.example.com>
2023-06-22 20:19:12 -0500

    Ejercicios resueltos

---
+++ 7-CICLOS 2/Ejercicio3.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Ejercicio3{
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Ingrese un numero entero: ");
+        int numero = entrada.nextInt();
+
+        int diferente = 0;
+        int contador = 1;
+
+        
+        for(int l=1; contador<=numero; l++){
+
+            diferente = (int)(Math.random()*99);
+            
+            int auxiliar = 0;
+
+            for(int i = 1; i <= diferente; i++){
+                if(diferente % i == 0){
+                    auxiliar = auxiliar +1;
+                }  
+            }
+
+            if(auxiliar==2){ 
+               
+                System.out.println("Primo "+contador+" generado es: "+diferente);
+                 contador = contador + 1;
+
+            }
+        }   
+    }
+}
