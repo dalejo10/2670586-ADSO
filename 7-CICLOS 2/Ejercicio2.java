92a9f52bcd0571b1aa246b27d73951714b1f645d
Fast <ejemplo.example.com>
2023-06-23 01:43:02 -0500

    Ejercicios resueltos

--- 7-CICLOS 2/Ejercicio2.java
+++ 7-CICLOS 2/Ejercicio2.java
@@ -1,29 +1,36 @@
-import java.util.Scanner;
 public class Ejercicio2{
     public static void main(String[] args){
-        Scanner entrada = new Scanner(System.in);
+        
 
-        System.out.print("La fecha generada es: ");
+        int dia = (int)(Math.random() * 31)+1;
+        int mes =  (int)(Math.random() * 12)+1;
+        int year =  (int)(Math.random() * 3000)+1;
 
-            int dia = (int)(Math.random() * 31);
-            if(dia>=1 && dia <=9){
-                 System.out.print("0"+dia);
-            }if(dia>9 && dia <=31){
-                System.out.print(dia);
+       
+        if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+            
+            mes =  (int)(Math.random() * 12)+1;
+             if (mes == 2) {
+                dia = (int)(Math.random() * 29) + 1;
+            }else{
+                dia = (int)(Math.random() * 30) + 1;
+            }if(mes==1 || mes==3 || mes==5 || mes==7 || mes==8 || mes==10 || mes==12){
+                dia = (int)(Math.random() * 31) + 1;  
             }
-
-            int mes =  (int)(Math.random() * 12);
-            if(mes>=1 && mes <=9){
-                 System.out.print("0"+mes);
-            }if(mes>9 && mes <=12){
-                System.out.print(mes);
+            System.out.print("La fecha generada es: "+dia+"/"+mes+"/"+year);
+            
+        }else{
+            mes = (int)(Math.random() * 12) + 1;
+            
+            if (mes == 2) {
+                dia = (int)(Math.random() * 28) + 1;
+            }else{
+                dia = (int)(Math.random() * 30) + 1;
+            }if(mes==1 || mes==3 || mes==5 || mes==7 || mes==8 || mes==10 || mes==12){
+                dia = (int)(Math.random() * 31) + 1;
             }
-
-            int year =  (int)(Math.random() * 9999);
-            if(year>=1000 && year <=9999){
-                 System.out.print(year);
-            }
-
+            System.out.print("La fecha generada es: "+dia+"/"+mes+"/"+year);
+        }
              
     }   
 }
